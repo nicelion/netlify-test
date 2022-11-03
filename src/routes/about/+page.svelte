4ff1cc1ba2fe5e2232170e4a7c31ad3b057e539c
Ian Thompson <ianthompson@nicelion.com>
2022-11-03 19:46:19 -0400

    update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -33,7 +33,7 @@
 	<input name="email" id="email" required placeholder="Email" type="email" />
 	<label for="message">Message</label>
 	<input name="message" id="message" required placeholder="Message" type="text" />
-	<input type="text" value="about" hidden>
+	<input type="text" id="location" value="about" hidden>
 
 	<input type="submit" value="Submit" />
   </form>
